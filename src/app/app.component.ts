ade54a777624727f87cbf60026fc7fc3ee0b79ba
JaiSai2003 <developer6256@gmail.com>
2025-04-15 10:59:44 +0530

    Routes (#3)
    * test commit
    
    * Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgforComponent } from './components/directives/ngfor/ngfor.component';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { DirectiveComponent } from './components/directives/directive/directive.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,DirectiveComponent],
+  imports: [RouterOutlet,RouterLink,DirectiveComponent],
   template: `
     <h1>{{ title }}</h1>
     <app-directive></app-directive>
-    <router-outlet></router-outlet>
+    <router-outlet />
   `,
+  // templateUrl: './app.component.html',
+  // styles: []
 })
 export class AppComponent {
+  constructor(private router: Router ) {}
+  navigateToHome() {
+    this.router.navigate(['/home']); 
+    // this.router.navigateByUrl('/home');
+  }
   title = 'practice';
 }
