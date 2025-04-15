6208ef98152201cf1f03226a9cdd374e0e63bee5
JaiSai2003 <developer6256@gmail.com>
2025-04-15 10:58:11 +0530

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { DirectiveComponent } from './components/directives/directive/directive.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home' , component: AdminComponent},
+    {path: 'dataBinding' , component: DataBindingComponent},
+    {path: 'directives' , component: DirectiveComponent}
+
+];
