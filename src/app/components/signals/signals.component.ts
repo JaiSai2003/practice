1a1128a53b4248e25c67a0acc1fa2dd020c824ff
JaiSai2003 <developer6256@gmail.com>
2025-04-17 17:13:43 +0530

    updated signals

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
-
+import {  Component, computed, effect, signal, ChangeDetectionStrategy} from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signals',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,6 +18,31 @@ export class SignalsComponent {
 
   number = signal<number>(0);
 
+
+
+
+  
+  quantity = signal<number>(1);
+  qtyAvailable = signal([1, 2, 3, 4, 5, 6]);
+
+
+  selectedVehicle = signal<Vehicle>({ id: 1, name: 'AT-AT', price: 10000 });
+
+  vehicles = signal<Vehicle[]>([]);
+
+  exPrice = computed(() => this.selectedVehicle().price * this.quantity());
+  color = computed(() => this.exPrice() > 50000 ? 'green' : 'blue');
+
+
+  // Example of a declarative effect
+  qtyEff = effect(() => console.log("Latest quantity:", this.quantity()));
+
+  onQuantitySelected(qty: number) {
+    this.quantity.set(qty);
+
+  }
+
+
   constructor() { 
     // const value = this.firstName();
     setTimeout(()=>{
@@ -25,6 +51,26 @@ export class SignalsComponent {
       // this.firstName.set('Sai');
       debugger;
     }, 10000)
+
+
+
+
+
+    console.log(this.quantity());
+
+    // Two for one sale
+    this.quantity.update((qty) => qty * 2);
+    // Interstellar price increase
+    this.selectedVehicle.update(v => ({
+      ...v,
+      price: v.price + (v.price * 0.2)
+    }));
+
+    // Add selected vehicle to array
+    this.vehicles.update(v => [...v, this.selectedVehicle()]);
+
+    // Example of an effect
+    effect(() => console.log(JSON.stringify(this.vehicles())));
   }
   
   increment(){
@@ -34,4 +80,13 @@ export class SignalsComponent {
   decrement(){
     this.number.update(val => val - 1);
   }
+
+
+
+}
+
+export interface Vehicle {
+  id: number;
+  name: string;
+  price: number;
 }
